2db1647ddf0a159bc48c136c3df656077e88eedc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-17 16:59:10 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,22 +11,27 @@
 
 int main(void)
 {
-	int k;
+	int i, j;
 
-	for (k = 1; k < 100; k++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(k + '0');
-		if (k < 10)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar('0');
+			putchar('0' + i);
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			if (i == 8 && j == 9)
+			{
+				putchar('\n');
+			}
 		}
-		if (!(k < 10 || k > 11) && (k < 20 || k > 22) && (k < 30 || k > 33)
-			&& (k < 40 || k > 44) && (k < 50 || k > 55) && (k < 60 || k > 66)
-			&& (k < 70 || k > 77) && (k < 80 || k > 88) && (k < 90 || k > 99))
-		putchar(',');
-		putchar(' ');
 	}
-	putchar('\n');
 
 	return (0);
 }
