d84ed8fdfe9e0a8546f79a5899543be12962a747
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-17 04:48:50 -0700

    number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Program to print single-digit
+ * combinations
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		putchar(k + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
