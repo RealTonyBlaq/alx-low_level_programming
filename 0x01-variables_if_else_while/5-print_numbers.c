142f54d11bc9a903e00ae8226163436c5a8149c1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-17 01:40:25 -0700

    Todays coding has been good

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: Printing single digit numbers
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 10; d++)
+	{
+		printf("%d\n", d);
+	}
+
+	return (0);
+}
