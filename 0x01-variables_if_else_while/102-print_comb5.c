930723ec418eaa9c528b66a76e9e0127984b25fa
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-18 15:29:04 -0700

    program to print two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Dscription: Program that prints the
+ * combination of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int j, k, l, m;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			putchar(' ');
+			for (l = 0; l < 10; l++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					putchar('0' + j);
+					putchar(k + '0');
+					putchar('0' + l);
+					putchar(m + '0');
+					if ((j != k || l != m) && (k != m || j != l))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		if (j == 9 && k == 8 && l == 9 && m == 9)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
