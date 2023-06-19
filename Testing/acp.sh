e350e45414889c105c621a2d98cb9ca00c3fa8b9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-19 14:23:31 -0700

    ''

--- Testing/acp.sh
+++ Testing/acp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git add . | read -p "Enter your commit message: " message | echo $message > git commit -m "'$message'" | git push
+git add . | read -p "Enter your commit message: " message | git commit -m "'$message'" | git push
