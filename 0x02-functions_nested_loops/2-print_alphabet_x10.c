d22a0e87dfc3cc29fcf724ff5fd1009927e32cff
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-20 16:04:36 -0700

    Printing 10 alphas with void declarations

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Declaring a function that
+ * prints alphas x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 0;
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
