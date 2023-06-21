7a828aff4ad1aa91415398c7aa3523434b541a12
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-21 05:35:30 -0700

    Fixed a bug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 	n = print_sign(n);
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(n + '0');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(n + '0');
 		return (0);
 	}
 	else
 	{
-		_putchar('0');
+		_putchar(n + '0');
 		return (-1);
 	}
 }
