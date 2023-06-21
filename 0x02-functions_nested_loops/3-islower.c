6143a13d85347c18cfcfaf8d6694b0b2b1777c3a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-21 02:04:41 -0700

    testing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	int _islower(int c);
+	int j = 1, k = 0;
+	char c = 'a';
+
+	if (_islower(c))
+	{
+		_putchar(j + '0');
+	}
+	else
+	{
+		_putchar(k + '0');
+	}
+
+	return (0);
+}
