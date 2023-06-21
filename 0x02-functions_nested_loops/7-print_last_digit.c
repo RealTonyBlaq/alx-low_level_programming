fe3b438c3112dd5536b74589901e9b981bedd60b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-21 08:40:55 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,7 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
-	{
-		_putchar('0' + (n % 10));
-	}
-	else if (n == 0)
-	{
-		_putchar(48);
-	}
-	else
-	{
-		_putchar('0' + (-n % 10));
-	}
-	return (0);
+	int d = n % 10;
+
+	return (d);
 }
