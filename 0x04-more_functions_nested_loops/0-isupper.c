fbb91c396f325851cafa0fc392feb74ee0917ab7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-23 15:15:09 -0700

    capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Defines uppercase letters
+ * @c: Character to be checked
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c == 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (c);
+}
