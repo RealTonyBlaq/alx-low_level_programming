cf2a477243d5058adba34702d6d5891404a25d92
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-22 23:59:40 -0700

    fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 /*	i = 0;*/
-/**
-*removing causes of loops - while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+
+/*removing causes of loops - while (i < 10)*/
+/*	{*/
+/*		putchar(i);*/
+/*	}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
