afcc06c7a4f365916181d28bf10b1d0a4cac3c2a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-23 17:52:06 -0700

    gg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,11 @@
 void more_numbers(void)
 {
 	int i, k;
+	int l = 10, m = 15;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < l; i++)
 	{
-		for (k = 0; k <= 14; k++)
+		for (k = 0; k < m; k++)
 		{
 			_putchar(k + '0');
 		}
