93784b2792fafa6e83ae8c4c87df2085e76b1afd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-25 05:25:53 -0700

    got time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,22 +9,17 @@
 void jack_bauer(void)
 {
 	int h, m;
+
+	for (h = 0; h < 24; h++)
 	{
-		for (h = 0; h <= 23; h++)
+		for (m = 0; m < 60; m++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			for (m = 0; m <= 59; m++)
-			{
-				_putchar(':');
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
-				_putchar('\n');
-				if ((h == 23) && (m == 59))
-				{
-				_putchar('\n');
-				}
-			}
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(58);
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 int add(int, int);
+void jack_bauer(void);
 
 #endif
