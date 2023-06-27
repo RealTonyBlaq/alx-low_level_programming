e5409465725d6a92aa540411395558364419fe75
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-27 13:22:09 -0700

    fixing strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,12 @@
 void puts2(char *str)
 {
 	long int k;
-	long int *m;
 
-	k = *str;
-	*m = k;
-	while (*m != '\0')
+	k = 0;
+	while (str[k] != '\0')
 	{
-		_putchar(*m);
-		m = m + 2;
+		_putchar(str[k]);
+		k += 2;
 	}
 	_putchar('\n');
 }
