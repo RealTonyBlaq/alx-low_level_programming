cd2875c3b5e94972a0ebaf99f60f1430307bd87b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-28 16:00:55 -0700

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonals
+ * @n: Numbers of times to print it
+ *
+ * Return: Nothing
+ */
+
+void print_diagonal(int n)
+{
+	int space;
+	int line;
+
+	if (n > 1)
+	{
+		for (line = 1; line <= n; line++)
+		{
+			if (line != 1)
+			{
+				for (space = 1; space <= line - 1; space++)
+				{
+					_putchar(32);
+				}
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
