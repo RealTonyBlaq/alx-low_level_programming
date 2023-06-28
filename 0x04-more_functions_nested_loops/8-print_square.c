282bca27dbdd54ef490794d7ad441b184d615536
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-06-28 13:45:08 -0700

    square is square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: Size of the square to be printed
+ *
+ * Return: Nothing
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (column = 1; column <= size; column++)
+		{
+			for (row = 1; row <= size; row++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
