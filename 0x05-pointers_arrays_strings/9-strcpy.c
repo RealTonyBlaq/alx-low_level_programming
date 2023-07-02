39dea5f88d412f954864483b6e87bf0200553cf9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-02 05:26:13 -0700

    String copy.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies Strings
+ * @dest: The destination
+ * @src: The source to be copied
+ *
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (p);
+	return (0);
+}
