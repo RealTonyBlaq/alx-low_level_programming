457e350479355168a1670ebb64e5bf2cbb9aaf67
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-03 07:01:34 -0700

    Filling a byte in memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the array
+ * @b: Characters to be filled
+ * @n: Number of times
+ *
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while ((s[i] != s[n - 1]) && i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+	return (0);
+}
