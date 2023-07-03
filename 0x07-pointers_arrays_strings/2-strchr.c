3ef7c360a5d1d21ebbec9c70e36223dcbc30f51c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-03 10:11:42 -0700

    string located

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strchr - Locates a character in a string
  * @s: The String to be checked
  * @c: The character to be located
- *
  * Return: Always 0, or "NULL" if otherwise
  */
 
@@ -19,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (NULL);
+	return (0);
 }
