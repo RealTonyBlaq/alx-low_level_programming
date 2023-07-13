25841a09db4681026945a27d43a936e312bded79
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-13 21:12:00 +0100

    function allocates memory to an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/**
+ * _memset - Copies chars
+ * @str: The string
+ * @b: The input to be copied
+ * @n: The number of bytes
+ * Return: Nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
 /**
  * _calloc - Function allocates memory to an array
  * @nmemb: The array
@@ -22,6 +44,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	array = calloc(nmemb, size);
+	_memset(array, 0, (nmemb * size));
 	return (array);
 }
