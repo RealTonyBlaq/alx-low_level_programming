2e33551a1b45c274bb8fdadece506442258d5a06
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-13 18:56:33 +0100

    function concats two strings

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Function allocates memory to an array
+ * @nmemb: The array
+ * @size: The size of the array
+ *
+ * Return: Nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
