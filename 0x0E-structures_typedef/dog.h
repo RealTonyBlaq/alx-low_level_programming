9f1c1c7387e1fa2de00f23cdb87c125a3a405bca
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-17 22:09:42 +0100

    created a struct in a header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The structure
+ * @name: A char type
+ * @age: A float type
+ * @owner: A char type
+ *
+ * Julien likes dogs and so do I
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* #ifndef DOG_H */
