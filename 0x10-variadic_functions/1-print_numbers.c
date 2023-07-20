bf7fabb2125729ab57d6d39e09f50f3bea498b6c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-20 22:06:29 +0100

    Printing numbers from function arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints numbers from parameters
+ * @separator: Char to be printed after an int
+ * @n: Number of integers passed to the function
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list var;
+
+	if (n <= 0)
+	{
+		return;
+	}
+	va_start(var, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(var, int));
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
