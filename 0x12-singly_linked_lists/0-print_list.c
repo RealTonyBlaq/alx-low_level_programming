64f79521776b9c4a68898a1229bc40a73feb7578
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-28 01:05:00 +0100

    Function prints the elements in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints the elements in a linked list
+ * @h: A pointer to a list
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	if (h == NULL)
+	{
+		return (-1);
+	}
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
