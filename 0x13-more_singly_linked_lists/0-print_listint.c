a079d856ce4361d42c3c18d8c24e242f71f86a0d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-01 23:30:40 +0100

    printing lists and returning node counts

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints a list
+ * @h: The head node
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (-1);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
