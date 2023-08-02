d0451b19f5fd4bef134f35f5a810fa7214421b34
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-02 02:36:07 +0100

    free memory flies like a bird

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees memory
+ * @head: The list
+ *
+ * Description: Function frees memory allocated
+ * to a listint_t list and sets head to NULL
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	while (*head != NULL)
+	{
+		list = *(head);
+		*head = list->next;
+		free(list);
+	}
+}
