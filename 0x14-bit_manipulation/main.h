858491866aa9d190cc0c4dd67f73c23e510770b7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-04 00:54:14 +0100

    printing in binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *str);
 int power(int base, int exponent);
+void print_binary(unsigned long int n);
 
 #endif /* #ifndef MAIN_H */
