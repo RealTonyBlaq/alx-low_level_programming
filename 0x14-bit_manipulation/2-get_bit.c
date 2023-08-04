dd35c4d601b3e16133bc0acb8152948b63d5609d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-04 01:09:58 +0100

    tells the Binary representation of a bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - Gets the value of a bit at a
+ * particular index.
+ * @n: The bit
+ * @index: The index, starting from 0
+ *
+ * Return: 0 or 1 - Value of the bit at index;
+ * otherwise -1.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	if ((n & (1 << index)) == 0)
+	{
+		return (0);
+	}
+	return (1);
+}
