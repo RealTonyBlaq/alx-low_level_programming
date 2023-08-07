fbc0c01ac2c1cf254590d60db7ac6defb8431e56
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-08 00:40:06 +0100

    function appends texts to textfiles

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,15 +14,39 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd == NULL)
+	if (text_content != NULL)
 	{
-		close(pd);
-		return (-1);
+		fd = open(filename, O_RDWR | O_APPEND);
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		n_bytes = write(fd, text_content, _strlen(text_content));
+		if (n_bytes < 0)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	n_bytes = write(filename, text_content, _strlen(text_content));
-	close(fd):
-	if (n_bytes < 0)
-		return (-1);
+	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - Gets a string length
+ * @str: The string
+ *
+ * Return: The length
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
