a6938ee7903603deeacd6d3d30c06da077f4d6f9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-08 00:43:17 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,16 +15,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		*text_content = ' ';
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
-		return (-1);
-	n_bytes = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (n_bytes < 0)
+	if (text_content != NULL)
 	{
-		return (-1);
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fd < 0)
+			return (-1);
+		n_bytes = write(fd, text_content, _strlen(text_content));
+		close(fd);
+		if (n_bytes < 0)
+		{
+			return (-1);
+		}
 	}
 	return (1);
 }
