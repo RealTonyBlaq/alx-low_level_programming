9d5e74ed8b2d3f4ed11e2bb1906915c6981a3d9b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-07 23:43:53 +0100

    creating files and writing content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,15 +15,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		*text_content = ' ';
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	n_bytes = write(STDIN_FILENO, text_content, _strlen(text_content));
+	n_bytes = write(fd, text_content, _strlen(text_content));
+	close(fd);
 	if (n_bytes == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	return (1);
