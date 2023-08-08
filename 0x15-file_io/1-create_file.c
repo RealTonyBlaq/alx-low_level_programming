68350d884a646670204220c80ca6e90219ec4283
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-08 11:00:13 +0100

    bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, n_bytes;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
+	if (!text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 400);
 	if (fd < 0)
