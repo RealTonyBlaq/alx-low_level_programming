4712ad78b24a988d4003b4ed3e1571ee4d2219f2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-08-08 16:06:39 +0100

    bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,38 +11,38 @@
 
 void cp(const char *file_from, const char *file_to)
 {
-	int fd_value, n_bytes, ret1, ret2;
+	int fd_value1, fd_value2, n_bytes, ret1, ret2;
 	char *text;
 
-	fd_value = open(file_from, O_RDONLY);
-	if ((fd_value < 0) || !file_from)
+	fd_value1 = open(file_from, O_RDONLY);
+	if ((fd_value1 < 0) || file_from == NULL)
 	{
-		close(fd_value);
+		close(fd_value1);
 		perror("Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
 	text = malloc(BYTES);
-	n_bytes = read(fd_value, text, BYTES);
-	if (n_bytes < 0)
+	n_bytes = read(fd_value1, text, BYTES);
+	if (n_bytes < 0 || text == NULL)
 	{
 		free(text);
-		close(fd_value);
+		close(fd_value1);
 		perror("Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	ret1 = close(fd_value);
-	fd_value = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+	fd_value2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	n_bytes = write(fd_value, text, n_bytes);
-	if (n_bytes < 0 || fd_value < 0)
+	n_bytes = write(fd_value2, text, n_bytes);
+	if (n_bytes < 0 || fd_value2 < 0 || !file_to)
 	{
 		free(text);
-		close(fd_value);
+		close(fd_value2);
 		perror("Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
-	ret2 = close(fd_value);
-	if (ret1 == -1 || ret2 == -1)
+	ret1 = close(fd_value1);
+	ret2 = close(fd_value2);
+	if (ret1 < 0 || ret2 < 0)
 	{
 		free(text);
 		perror("Error: Can't close fd FD_VALUE\n");
