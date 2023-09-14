c4453ccef77bbd9659e937c42690b3efa0fd888b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-14 22:12:23 +0100

    function inserts a node at idx

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node at index(idx)
+ * @h: The list
+ * @idx: The index
+ * @n: The data(n) of the new node
+ *
+ * Return: The address of the new node, else NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL, *tmp = *h, *ptr = NULL;
+	unsigned int count, i = 0;
+
+	if (h == NULL)
+		return (NULL);
+	count = node_count(tmp);
+	if (idx > count)
+		return (NULL);
+	if (*h != NULL)
+	{
+		ptr = *h;
+		while (ptr->next != NULL && i < idx)
+		{
+			ptr = ptr->next;
+			i++;
+		}
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->prev = ptr;
+		new->next = ptr->next;
+		if (ptr->next != NULL)
+			ptr->next->prev = new;
+		ptr->next = new;
+		return (new);
+	}
+	return (NULL);
+}
+
+
+/**
+ * node_count - Counts the number of nodes in a list
+ * @head: The list
+ *
+ * Return: The count
+ */
+unsigned int node_count(dlistint_t *head)
+{
+	unsigned int nodes = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		nodes++;
+		current = current->next;
+	}
+	return (nodes);
+}
