d4770de16b678e24f0ceacc74af602be2605f00e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-14 22:57:56 +0100

    function inserts a node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new = add_dnodeint(h, n);
 			return (new);
 		}
+		else if (idx == count)
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
 		ptr = *h;
 		while (ptr->next != NULL && i < idx)
 		{
