32c991a3ee6ab66070a159790348d235ead0f7aa
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-15 00:58:09 +0100

    function inserts a node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,39 +15,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	if (*h != NULL)
+	count = node_count(tmp);
+	if (idx > count - 1)
+		return (NULL);
+	if (idx == 0)
 	{
-		count = node_count(tmp);
-		if (idx > count - 1)
-			return (NULL);
-		if (idx == 0)
-		{
-			new = add_dnodeint(h, n);
-			return (new);
-		}
-		ptr = *h;
-		while (ptr != NULL && i < idx)
-		{
-			ptr = ptr->next;
-			i++;
-		}
-		if (ptr == NULL)
-			return (NULL);
-		if (ptr->next == NULL)
-		{
-			new = add_dnodeint_end(h, n);
-			return (new);
-		}
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		new->prev = ptr;
-		new->next = ptr->next;
-		ptr->next->prev = new;
-		ptr->next = new;
+		new = add_dnodeint(h, n);
 		return (new);
 	}
+	ptr = *h;
+	while (ptr != NULL && i < idx)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	if (ptr == NULL)
+		return (NULL);
+	if (ptr->next == NULL)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = ptr;
+	new->next = ptr->next;
+	ptr->next->prev = new;
+	ptr->next = new;
+	return (new);
 	return (NULL);
 }
 
