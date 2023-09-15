058838d5dcc841bc3d07deb4dbaf8067be5cea27
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-15 01:05:04 +0100

    function inserts a node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count, i = 0;
 
 	count = node_count(tmp);
-	if (idx > count - 1)
+	if (idx > count)
 		return (NULL);
 	if (idx == 0)
 	{
