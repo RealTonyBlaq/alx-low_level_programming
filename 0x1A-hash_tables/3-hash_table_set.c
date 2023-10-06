1f8989a931d8ab7cde3dbbce0606f9e76d7a68a6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-06 19:51:56 +0100

    func creates a new instance of a key at index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,9 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	if (ht->array[index] == NULL)
-	{
 		ht->array[index] = new;
-	}
 	else
 	{
 		temp = ht->array[index];
