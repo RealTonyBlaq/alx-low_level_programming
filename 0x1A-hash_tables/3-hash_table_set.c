38530fd929be3bbd30aa61ad7e311526eafab1a0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-06 19:28:44 +0100

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,17 +10,39 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index, i;
 	hash_node_t *new, *temp = NULL;
 
-	if (ht == NULL || !key || !value)
+	if (ht == NULL || !key || *key == '\0' || !value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
+	i = index;
+	while (ht->array[i])
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+		i++;
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
 	new->next = NULL;
 	if (ht->array[index] == NULL)
 	{
@@ -29,16 +51,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		temp = ht->array[index];
-		if (strcmp(temp->key, key) == 0)
-		{
-			strcpy(temp->value, value);
-			free(new);
-		}
-		else
-		{
-			new->next = temp;
-			temp = new;
-		}
+		new->next = temp;
+		temp = new;
 	}
 	return (1);
 }
