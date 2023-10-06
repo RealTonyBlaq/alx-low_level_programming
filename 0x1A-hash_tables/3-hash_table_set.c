2e8a2591f114f29e785941359db28e9a932f32f7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-10-06 18:15:09 +0100

    insertion or updation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,9 +13,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new, *ptr = NULL, *temp = NULL;
 
-	if (ht == NULL || key == "")
+	if (ht == NULL || strcmp(key, "") == 0)
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
