bd4e71d2ab0e49674e64519b490fe63bbc3e7875
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-09 00:58:44 -0800

    buidl

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,12 +16,19 @@ def island_perimeter(grid):
     """
     if grid is None:
         return 0
-    if not isinstance(grid, list):
+    if not isinstance(grid, list) or len(grid) == 0:
         return 0
-    count = 0
-    for row in grid:
-        for i in row:
-            if i == 1:
-                count += 1
-    count *= 2
-    return count
+    height = 0
+    width = 0
+    r_size = len(grid[0])
+    for i in range(len(grid)):
+        for k in range(len(grid[i])):
+            if grid[i][k] == 1:
+                width += 1
+                break
+    for i in range(r_size):
+        for row in grid:
+            if row[i] == 1:
+                height += 1
+                break
+    return 2 * (width + height)
