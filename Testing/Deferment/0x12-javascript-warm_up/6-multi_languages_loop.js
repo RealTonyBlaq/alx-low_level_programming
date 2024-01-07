dc2b9267a58fedb373a18b783e9c8a9abef6879d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-07 15:59:21 +0100

    new database

---
+++ Testing/Deferment/0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Script uses a loop and an array to print strings */
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < myArray.length; index++) {
+  console.log(myArray[index]);
+}
