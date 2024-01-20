8fed84748267e913f85cabacc15444e5f5a52b07
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-20 21:59:14 +0100

    new

---
+++ Testing/Deferment/0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Module """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """
+    Defining the class City that links to the sql table 'cities'
+
+    Class Attributes:
+    -----------------
+
+    __tablename__ (str): name of the table
+    id (int): An auto-generated column with unique values, a primary key
+            and non-null
+    name (str): A string column of max 128 characters and cannot be null
+    state_id (int): A column that cannot be null and is a foreign key
+            to states.id
+    """
+    __tablename__ = 'cities'
+    id = Column('id', Integer, nullable=False, primary_key=True, unique=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'),
+                      nullable=False)
+    state = relationship("State", back_populates="cities")
