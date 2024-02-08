a438cd1ede60e1e02596e4e93df1f91684396122
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-09 00:16:23 +0100

    header update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void my_print(int *array, size_t start, size_t finish);
 
 #endif /* SEARCH_ALGOS */
