2ba8f45eec2447c1aa376114a057d548e0478145
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-08 21:02:22 +0100

    linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
         return(-1);
     while (i < size)
     {
-        printf("Value checked array[0] = [10]", )
+        printf("Value checked array[%d] = [%d]", i, array[i])
         if (array[i] == value)
             return (1);
         i++;
