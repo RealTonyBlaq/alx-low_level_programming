f6912506bda4b5b44c23b97b2efca597edb0c431
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-08 20:35:56 +0100

    linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
-* int linear_search - 
+* int linear_search - Searches an array for a value using the
+                        linear search algorithm
 */
 
 int linear_search(int *array, size_t size, int value)
