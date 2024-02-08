956b699135d071c56dbe161e1a37a536e9d7a345
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-08 20:55:02 +0100

    linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,8 +15,8 @@ int linear_search(int *array, size_t size, int value)
 
     if (array == NULL || size == 0)
         return(-1);
-    while (array[i])
+    while (i < size)
     {
-        if 
+        if ()
     }
 }
