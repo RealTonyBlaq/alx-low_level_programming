51c32e21114e8dcc328bc4088bdc3ff3a19e538e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 18:50:48 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -24,7 +24,8 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[jump_point] > value)
 			return (lin_search(array, 1, jump_point - 1, value));
-		else
+		else if (array[jump_point] < value)
+			return (lin)
 	}
 }
 
