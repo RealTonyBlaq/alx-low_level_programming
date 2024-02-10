efab69704fbec2df072c85fc5b1fc78f2279dd8e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 18:45:49 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -42,7 +42,7 @@ int lin_search(int *array, size_t start, size_t finish, int v)
 
 	for (i = start; i <= finish; i++)
 	{
-		printf("Value checked array[%d] = %d\n", 0, array[0])
+		printf("Value checked array[%lu] = %d\n", i, array[i])
 		if (array[i] == v)
 			return (i);
 	}
