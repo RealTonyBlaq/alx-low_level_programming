52eaf4e02e7792fb1fd156a4f913d3b1541ad0e8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 20:54:19 +0100

    added new prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void my_print(int *array, size_t start, size_t finish);
+int lin_search(int *array, size_t start, size_t finish, int v);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS */
