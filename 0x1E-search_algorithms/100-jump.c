f599f77d98aa2e05cb1a00e4f3a0aec456aa65de
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 18:14:49 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -4,5 +4,11 @@
 /**
 * jump_search - Searches for an element in an array using
 *				the jump search algorithm
+* @array: The array
 * 
 */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i;
+}
