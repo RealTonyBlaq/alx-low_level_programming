77a6f652fb4b52a3eb9e02eaea12c5246db1327b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 18:57:07 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -24,7 +24,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[jump_point] > value)
 		{
-			printf()
+			printf("")
 			return (lin_search(array, 1, jump_point - 1, value));
 		else if (array[jump_point] < value)
 			return (lin_search(array, jump_point + 1, size, value));
