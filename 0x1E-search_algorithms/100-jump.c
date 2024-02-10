aecffc66e8cb675222e394d4afb69efdd2fff9ba
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-10 19:03:01 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,9 +25,10 @@ int jump_search(int *array, size_t size, int value)
 		if (array[jump_point] > value)
 		{
 			printf("Value found between index [%lu] and [%lu]", 1, jump_point);
-			
+			break;
 		}
 	}
+	return (lin_search(array, 1, jump_point - 1, value));
 }
 
 /**
