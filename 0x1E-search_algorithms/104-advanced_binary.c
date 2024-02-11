d174c5a9e25fd03f51b9af330c0581900a600e94
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-12 00:09:34 +0100

    advanced binary search algo

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - Algorthis searches for a value using the
+ *			advanced binary search algorithm.
+ * @array: The array
+ * @size: Size of the array
+ * @value: Value to be searched
+ *
+ * Return: The index where the element was found,
+ *		otherwise -1 is returned.
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t start = 0, finish = size - 1, mid;
+
+	if (array)
+	{
+		while (start <= finish)
+		{
+			mid = start + (finish - start) / 2;
+			my_print(array, start, finish);
+			if (array[mid] > value)
+				finish = mid - 1;
+			else if (array[mid] < value)
+				start = mid + 1;
+			else
+				return (mid);
+		}
+	}
+	return (-1);
+}
+
+/**
+ * my_print - Prints the array with a format
+ * @array: The array
+ * @start: The first index of the array
+ * @finish: The last index of the array
+ *
+ * Return: Nothing
+ */
+
+void my_print(int *array, size_t start, size_t finish)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= finish; i++)
+	{
+		printf("%d", array[i]);
+		if (i != finish)
+			printf(", ");
+	}
+	printf("\n");
+}
