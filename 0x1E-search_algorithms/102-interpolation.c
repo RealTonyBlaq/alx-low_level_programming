684acc714a590ac70914bac79b7e49c5fac9117d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-11 12:19:20 +0100

    interpolation search build

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,6 +13,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t i, pos, low, high;
 	
+	high = size, low = 0;
 	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-	
+
 }
