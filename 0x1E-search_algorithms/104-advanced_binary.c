b0fe5dd3b183613094e36deb2d1bff3ab47e3229
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-12 00:46:24 +0100

    advanced binary search algo

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -34,7 +34,7 @@ int bin_recursive(int *array, size_t start, size_t finish, int value)
 		else if (array[mid] < value)
 		{
 			start = mid + 1;
-			return ((array, start, finish, value));
+			return (bin_recursive(array, start, finish, value));
 		}
 		else
 			return (mid);
