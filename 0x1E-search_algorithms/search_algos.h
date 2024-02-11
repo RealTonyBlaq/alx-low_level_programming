7bf2b51fe8d6315056be5f7aa7aa1338d5267d5b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-11 13:39:13 +0100

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,7 @@ void my_print(int *array, size_t start, size_t finish);
 int lin_search(int *array, size_t start, size_t finish, int v);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int bin_search(int *array, size_t start, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS */
