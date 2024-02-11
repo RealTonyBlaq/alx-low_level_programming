25b099c770f9c3b7882a9127ab69a98ee0239b27
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-11 13:20:09 +0100

    exp search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -15,3 +15,47 @@ int exponential_search(int *array, size_t size, int value)
 
 	
 }
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0, finish = size - 1, mid;
+
+	if (array)
+	{
+		while (start <= finish)
+		{
+			mid = start + (finish - start) / 2;
+			my_print(array, start, finish);
+			if (array[mid] > value)
+				finish = mid - 1;
+			else if (array[mid] < value)
+				start = mid + 1;
+			else
+				return (mid);
+		}
+	}
+	return (-1);
+}
+
+/**
+ * my_print - Prints the array with a format
+ * @array: The array
+ * @start: The first index of the array
+ * @finish: The last index of the array
+ *
+ * Return: Nothing
+ */
+
+void my_print(int *array, size_t start, size_t finish)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= finish; i++)
+	{
+		printf("%d", array[i]);
+		if (i != finish)
+			printf(", ");
+	}
+	printf("\n");
+}
