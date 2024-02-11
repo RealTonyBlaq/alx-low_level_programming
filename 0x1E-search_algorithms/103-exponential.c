f7ea60735fe465cd977916b8330fd1432cd65267
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-11 13:17:15 +0100

    exp search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,8 @@
 #include "search_algos.h"
 
 /**
- * exponential_search 
+ * exponential_search - Finds an element in an array using
+ *						the exponential search algorithm
+ * @arrat
 */
 int exponential_search(int *array, size_t size, int value)
