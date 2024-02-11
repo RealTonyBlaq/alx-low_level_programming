c5abdba5c202c6a9da6f2da47242134c981f9da6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-11 12:14:58 +0100

    interpolation search build

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -3,7 +3,9 @@
 /**
  * interpolation_search - Finds an element in an array using the
  *							interpolation search algorithm
- * @arra
+ * @array: The array
+ * @size: The length of the array
+ * @value: The value to be sought
 */
 int interpolation_search(int *array, size_t size, int value)
 {
