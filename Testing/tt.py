bcb2da5351c3d5f87193f73c4660ef12531c592c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-10 23:31:05 +0100

    test

--- Testing/tt.py
+++ Testing/tt.py
@@ -8,4 +8,5 @@ app = Flask(__name__)
 @app.route('/api/stat', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns the api status """
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}), 200
+
