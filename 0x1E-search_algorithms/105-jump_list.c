2c4e42ed1525acb68b89ad67ceeb5b4bbe9cddc5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-24 18:05:31 +0100

    jump list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -55,7 +55,8 @@ listint_t *point(listint_t *list, size_t index)
  * list_search - Searches a list from index a to b
  * @list: The list
  * @a: Index a
- * 
+ * @b: Index b
+ * @value: 
 */
 listint_t *list_search(listint_t *list, size_t a, size_t b, int value)
 {
