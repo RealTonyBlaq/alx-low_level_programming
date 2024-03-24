96a56716a66c13baf445f9962d4d6eda18960913
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-24 17:21:52 +0100

    jump list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,14 +12,14 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	listint_t *current = list, listint_t *ptr;
+	listint_t *current = list, *ptr;
 	size_t a = 0, b = 0;
 
 	if (!list || size <= 0)
 		return (NULL);
 	while (current->n < value)
 	{
-
+		ptr = poin
 		printf("Value checked array[%lu] = [%d]\n", b, point(list, b) ? );
 	}
 }
