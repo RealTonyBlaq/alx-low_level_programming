9d1642bd7293b01594b136afb9fd3242dfe03014
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-24 18:01:54 +0100

    jump list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -50,6 +50,10 @@ listint_t *point(listint_t *list, size_t index)
 	return (NULL);
 }
 
+
+/**
+ * list_
+*/
 listint_t *list_search(listint_t *list, size_t a, size_t b, int value)
 {
 	listint_t *ptr;
