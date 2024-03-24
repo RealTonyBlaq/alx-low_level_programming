1818151aaac47f5c750453a24687a09c7103aeb0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-24 16:21:14 +0100

    jump list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,9 +1,9 @@
 #include "search_algos.h"
 
 /**
- * jump_list - 
+ * jump_list - searches for a value in a sorted list of integers using the Jump search algorithm
 */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    listint_t *head;
+    listint_t *current;
 }
