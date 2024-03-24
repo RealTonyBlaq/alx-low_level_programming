4f61c9b8863b1aa06ae14dfbd8194839ea641acd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-24 19:19:27 +0100

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -34,5 +34,8 @@ int bin_recursive(int *array, size_t start, size_t size, int value);
 void print_list(const listint_t *list);
 void free_list(listint_t *list);
 listint_t *create_list(int *array, size_t size);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *list_search(listint_t *list, size_t a, size_t b, int value);
+listint_t *jump(listint_t *list, size_t index);
 
 #endif /* SEARCH_ALGOS */
